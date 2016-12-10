03e5597412f221a0141ef81c41933017d3a8ed97
Kale Kundert <kale@thekunderts.net>
2016-12-10 10:43:04 -0800

    Add the --fraction option to the gc command.

--- kbkdna/cli.py
+++ kbkdna/cli.py
@@ -6,15 +6,20 @@ Perform various simple manipulations on DNA sequences.
 Usage:
     dna len <seq>
     dna rc <seq>
-    dna gc <seq>
+    dna gc <seq> [--fraction]
 
 Commands:
-    len: Print the length of the given DNA sequence.
-    rc:  Print the reverse-complement of the given DNA sequence.
-    gc:  Print the GC content of the given DNA sequence.
+    len:  Print the length of the given DNA sequence.
+    rc:   Print the reverse-complement of the given DNA sequence.
+    gc:   Print the GC content of the given DNA sequence.
 
 Arguments:
-    <seq>: The DNA sequence to work with.
+    <seq>:  The DNA sequence to work with.
+
+Options:
+    --fraction 
+        For the 'gc' command, display the result as a raw fraction
+        (e.g. a number between 0 and 1) rather than a percentage.
 """
 
 from . import dna
@@ -29,5 +34,7 @@ def main():
     if args['rc']:
         print dna.reverse_complement(seq)
     if args['gc']:
-        print '{.2f}\%'.format(dna.gc_content(seq) / 100)
+        gc = dna.gc_content(seq)
+        if args['--fraction']: print gc
+        else: print '{.2f}\%'.format(gc / 100)
 
